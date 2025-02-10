8f72b8f5dbc91546b929c65ea699015c659d2030
Chrystine Martins <chrystine.martins@outlook.com>
2025-02-10 20:52:31 -0300

    add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logoSpotify from '../assets/logo/spotify-logo.png';
+
+const Header = () => {
+
+    return (
+      
+      <div>
+          
+            <img src={logoSpotify} alt="Logo Spotify" />
+
+
+    </div>
+  )
+}
+
+export default Header
