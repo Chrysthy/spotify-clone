506211226bf761e44f0acb3439abff5bcd8bca4c
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2025-02-11 13:43:01 -0300

    add single item and font awesome

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
 //import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Main = () => {
 
@@ -13,6 +14,19 @@ const Main = () => {
                     <h2>Artistas Populares</h2>
 
                     <a className="item-list__link" href="/">Mostrar tudo</a>
+
+                </div>
+
+
+                <div className="single-item" >
+
+                    <img className="single-item__image"  src="https://i.scdn.co/image/ab67616100005174fde13b4ff04ee1c0f33c6878" alt="Imagem do Artista X"/>
+
+                    <FontAwesomeIcon icon="fa-solid fa-circle-play" />
+
+                    <p>Luan Santana</p>
+
+                    <p>Artista</p>
 
                 </div>
 
