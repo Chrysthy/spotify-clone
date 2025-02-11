ee01f42e3c752daba771639ca1f087927e7dbeb8
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2025-02-11 13:27:25 -0300

    add new classNames

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,25 @@
 //import React from 'react'
 
 const Main = () => {
-    return (
-      
-        <div className="main__texts">
-           
-            <h2>Artistas Populares</h2>
 
-            <a className="main__link" href="/">Mostrar tudo</a>
-        
+    return (
+
+        <div className="main">
+
+            <div className="item-list">
+
+                <div className="item-list__header">
+
+                    <h2>Artistas Populares</h2>
+
+                    <a className="item-list__link" href="/">Mostrar tudo</a>
+
+                </div>
+
+            </div>
+
         </div>
-  )
+    )
 }
 
 export default Main
