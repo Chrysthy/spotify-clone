0c4f869f848e3809ac227d62c97fdc1b82585839
Chrystine Martins <chrystine.martins@outlook.com>
2025-02-10 21:03:42 -0300

    add Head component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+//import React from 'react';
+import Header from './components/Header';
+
+
 function App() {
 
   return (
+
     <>
 
+    <Header/>
+
+      
     </>
   )
 }
 
-export default App
+export default App;
