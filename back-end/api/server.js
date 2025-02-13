b83a0588b0e304708004d1b009ed3e8bce03e52c
Chrystine Martins <chrystine.martins@outlook.com>
2025-02-13 19:56:24 -0300

    add server.js

---
+++ back-end/api/server.js
@@ -0,0 +1,10 @@
+//API  significa Application Programming Interface
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    
+})
