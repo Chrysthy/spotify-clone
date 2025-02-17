9430889884d5686e6e71c46159ad184cf9ca3290
Chrystine Martins <chrystine.martins@outlook.com>
2025-02-16 21:47:43 -0300

    add app.get

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,10 +1,17 @@
 //API  significa Application Programming Interface
+//Post, Get, Put, Delete
+//CRUD - Create, Read, Update, Delete
+//Endpoint - URL, onde a API pode ser acessada por uma aplicação
 import express from 'express';
 
 const app = express();
 const PORT = 3000;
 
+app.get('/', (request, response) => {
+    response.send("Hello World");
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    
+
 })
