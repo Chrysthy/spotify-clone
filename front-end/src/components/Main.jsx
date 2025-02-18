53ab7faa4c33ab66855071c7a9cc968af49d6557
Chrystine Martins <chrystine.martins@outlook.com>
2025-02-17 21:48:24 -0300

    add artists ans songs

--- front-end/src/components/Main.jsx
+++ front-end/src/components/Main.jsx
@@ -1,5 +1,7 @@
 //import React from 'react'
 import ItemList from "./ItemList";
+import { artistArray } from "../assets/database/artists";
+import { songsArray } from "../assets/database/songs";
 
 const Main = () => {
 
@@ -7,9 +9,9 @@ const Main = () => {
 
         <div className="main">
 
-            <ItemList title="Artistas" items={5} />
+            <ItemList title="Artistas" items={5} itemsArray={artistArray} />
 
-            <ItemList title="Músicas" items={10}/>
+            <ItemList title="Músicas" items={10} itemsArray={songsArray}/>
 
         </div>
     )
