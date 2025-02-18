ec7b5d9dac4be56002d8f5b63c0e4b4ebc6099b4
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2025-02-18 13:32:44 -0300

    add idPath

--- front-end/src/components/Main.jsx
+++ front-end/src/components/Main.jsx
@@ -9,9 +9,9 @@ const Main = () => {
 
         <div className="main">
 
-            <ItemList title="Artistas" items={5} itemsArray={artistArray} path='/artists' />
+            <ItemList title="Artistas" items={5} itemsArray={artistArray} path="/artists" idPath="/artist" />
 
-            <ItemList title="Músicas" items={10} itemsArray={songsArray} path='songs' />
+            <ItemList title="Músicas" items={10} itemsArray={songsArray} path="songs" idPath="/song"  />
 
         </div>
     )
