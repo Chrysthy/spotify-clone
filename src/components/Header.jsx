97f3c48d730f2288b753b17314dab746eac1558c
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-05 21:35:07 -0300

    update content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,15 @@ import logoSpotify from '../assets/logo/spotify-logo.png';
 
 const Header = () => {
     return (
-        <div>
+        <div className="header"> 
 
-            <img src={logoSpotify} alt="" />
+            <img src={logoSpotify} alt="Logo do Spotify" />
+
+            <a href="/" className="header__link">
+
+                <h1>Spotify</h1>
+
+            </a>
 
         </div>
     )
