4d5ff9e15b3c9d60243791e9f9d3731ca103321d
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-06 21:47:44 -0300

    add single item, img and p content

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,6 +13,14 @@ const Main = () => {
                     <a className='item-list__link' href="/">Mostrar tudo</a>
                 </div>
 
+                <div className='single-item'>
+
+                    <img className='single-item__image' src="https://i.scdn.co/image/ab67616100005174b97627e4ea832ed67617627a" alt="Imagem do artista X" />
+
+                    <p>Matheus & Kauan</p>
+
+                </div>
+
             </div>
 
         </div>
