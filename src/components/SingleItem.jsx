ef0394fa22bb2e3d44be8da07c1ec7d9d7d3799a
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-11 20:57:04 -0300

    add single item code

--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
 
 const SingleItem = () => {
     return (
-        <div>SingleItem</div>
+
+        <div className='single-item'>
+
+            <div className='single-item__div-image-button'>
+
+                <div className='single-item__div-image'>
+
+                    <img className='single-item__image' src="https://i.scdn.co/image/ab67616100005174b97627e4ea832ed67617627a" alt="Imagem do artista X" />
+
+                </div>
+
+                <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
+
+            </div>
+
+            <div className='single-item__texts'>
+
+                <div className='single-item__2lines'>
+
+                    <p className='single-item__title'>Matheus & Kauan</p>
+
+                </div>
+
+                <p className='single-item__type'>Artista</p>
+
+            </div>
+
+        </div>
     )
 }
 
