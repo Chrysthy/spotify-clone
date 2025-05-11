05130fccf964749655cfa987ccd31bc3a866d0b2
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-11 20:56:53 -0300

    Refactor code

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import SingleItem from './SingleItem';
 
 function ItemList() {
 
     return (
-        
+
         <div className='item-list'>
 
             <div className='item-list__header'>
@@ -17,33 +16,10 @@ function ItemList() {
 
             <div className='item-list__container'>
 
-                <div className='single-item'>
-
-                    <div className='single-item__div-image-button'>
-
-                        <div className='single-item__div-image'>
-
-                            <img className='single-item__image' src="https://i.scdn.co/image/ab67616100005174b97627e4ea832ed67617627a" alt="Imagem do artista X" />
-
-                        </div>
-
-                        <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
-
-                    </div>
-
-                    <div className='single-item__texts'>
-
-                        <div className='single-item__2lines'>
-
-                            <p className='single-item__title'>Matheus & Kauan</p>
-
-                        </div>
-
-                        <p className='single-item__type'>Artista</p>
-
-                    </div>
-
-                </div>
+                <SingleItem />
+                <SingleItem />
+                <SingleItem />
+                <SingleItem />
 
             </div>
         </div>
