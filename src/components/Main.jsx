ceb0aba28f0ed6b34dc538b6771824ae389c0e89
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-16 21:48:38 -0300

    update content

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React from "react";
 import ItemList from "./ItemList";
+import { artistArray } from "../assets/database/artists";
+import { songsArray } from "../assets/database/songs";
 
 const Main = () => {
+
     return (
-        <div className='main'>
 
-            <ItemList title="Artistas" items={5} />
+        <div className="main">
+            {/* Item List de Artistas */}
+            <ItemList
+                title="Artistas"
+                items={10}
+                itemsArray={artistArray}
+                path="/artists"
+                idPath="/artist"
+            />
 
-            <ItemList title="Músicas" items={10} />
+            {/* Item List de Músicas */}
+            <ItemList
+                title="Músicas"
+                items={20}
+                itemsArray={songsArray}
+                path="/songs"
+                idPath="/song"
+            />
 
         </div>
-    )
-}
 
-export default Main
+    );
+};
+
+export default Main;
