dc3604a3ed845c351411b0759a02562325004d07
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-16 21:48:44 -0300

    update content

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,28 +1,39 @@
-import React from 'react'
-import SingleItem from './SingleItem';
-import { artistArray } from '../assets/database/artists';
-import { songsArray } from '../assets/database/songs';
+import React from "react";
+import SingleItem from "./SingleItem";
+import { Link } from "react-router-dom";
 
-const ItemList = ({ title, items }) => {
-
+const ItemList = ({ title, items, itemsArray, path, idPath }) => {
     return (
 
-        <div className='item-list'>
+        <div className="item-list">
 
-            <div className='item-list__header'>
+            <div className="item-list__header">
 
-                <h2>{title} Populares</h2>
+                <h2>{title} populares</h2>
 
-                <a className='item-list__link' href="/">Mostrar tudo</a>
-            </div>
-
-            <div className='item-list__container'>
-
-                {artistArray.map((currentValue, index) => <SingleItem key={`${title}-${index}`} />)}
+                <Link to={path} className="item-list__link">
+                    Mostrar tudo
+                </Link>
 
             </div>
+
+            <div className="item-list__container">
+
+                {itemsArray
+                    .filter((currentValue, index) => index < items)
+                    .map((currObj, index) => (
+                        <SingleItem
+                            idPath={idPath}
+                            {...currObj}
+                            key={`${title}-${index}`}
+                        />
+                    ))}
+                
+            </div>
+
         </div>
-    )
-}
+        
+    );
+};
 
 export default ItemList;
