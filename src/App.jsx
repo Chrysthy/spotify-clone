f3fbcc224cbb4c704e0b0d0b2f29e832f1067502
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-18 22:17:37 -0300

    import pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
-import Main from './components/Main';
 import { BrowserRouter, Routes, Route } from 'react-router-dom/BrowserRouter';
+import Home from './pages/Home';
+import Artist from './pages/Artist';
+import Artists from './pages/Artists';
+import Song from './pages/Song';
+import Songs from './pages/Songs';
+
 
 const App = () => {
   return (
@@ -10,7 +15,18 @@ const App = () => {
 
       <Header />
 
-      <Main />
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/artists" element={<Artists />} />
+        <Route path="/artist" element={<Artist />} />
+        <Route path="/songs" element={<Songs />} />
+        <Route path="/song" element={<Song />} />
+
+        <Main />
+
+      </Routes>
 
     </BrowserRouter>
   )
