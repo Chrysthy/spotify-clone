1cad2c41932141933769258cb1e1a1859905bad8
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-18 22:12:15 -0300

    add BrowserRouter, Routes, Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
+import { BrowserRouter, Routes, Route } from 'react-router-dom/BrowserRouter';
 
 const App = () => {
   return (
-    <>
+
+    <BrowserRouter>
+
       <Header />
 
       <Main />
 
-    </>
+    </BrowserRouter>
   )
 }
 
