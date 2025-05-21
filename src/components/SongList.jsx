f6c1d414038747a09c85914e368d40f22ea91515
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-21 20:59:54 -0300

    add useState and onClick

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import SongItem from './SongItem'
+import { useState } from 'react';
 
 
 const SongList = ({ songsArray }) => {
 
-    const items = 5;
+    const [items, setItems] = useState(5);
 
     return (
 
@@ -12,7 +13,7 @@ const SongList = ({ songsArray }) => {
 
             {songsArray.filter((currentValue, index) => index < items).map((currentObj, index) => (<SongItem {...currentObj} index={index} key={(index)} />))}
 
-            <p className='song-list__see-more'>Ver mais</p>
+            <p className='song-list__see-more' onClick={() => { setItems(items + 5) }}>Ver mais</p>
 
         </div>
     )
