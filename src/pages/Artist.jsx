5e493c65d61c9250aa79ffbc3f8e20309dc6d75d
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-20 21:01:13 -0300

    add name of the artist

--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -8,7 +8,16 @@ const Artist = () => {
 
         <div className='artist'>
 
-            <div className="artist__header" style={{ backgroundImage: "linear-gradient(to bottom, var(--_shade), var(--_shade)), url(https://i.scdn.co/image/ab67618…)" }}></div>
+            <div
+                className="artist__header"
+                style={{
+                    backgroundImage:
+                        "linear-gradient(to bottom, var(--_shade), var(--_shade)), url(https://i.scdn.co/image/ab67618600001016e2952bec9cfd4e6862e23607)"
+                }}
+            >
+                <h2 className='artist__title'>Jorge & Mateus</h2>
+
+            </div>
 
             <div className="artist__body"></div>
 
