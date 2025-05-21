34b978b022b65b84d0ce6e079c91aaabecb336f9
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-21 19:20:29 -0300

    add fontawesome icons for player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay, faBackwardStep, faForwardStep } from '@fortawesome/free-solid-svg-icons';
 
 const Player = () => {
     return (
-        <div className='player'>Player</div>
+        <div className='player'>
+
+            <div className="player__controllers">
+
+                <FontAwesomeIcon
+                    className="palyer__icon"
+                    icon={faBackwardStep}
+                />
+                
+                <FontAwesomeIcon
+                    className="palyer__icon"
+                    icon={faCirclePlay}
+                />
+
+                <FontAwesomeIcon
+                    className="palyer__icon"
+                    icon={faForwardStep}
+                />
+
+            </div>
+
+            <div className="player__bar">
+
+
+            </div>
+
+
+        </div>
     )
 }
 
