514acf8dbfbebdcbb619c960c7bfc04048e3bea0
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-21 19:07:32 -0300

    add song container

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Song = () => {
     return (
-        <div>Song</div>
+        <div className='song'>
+
+            <div className="song__container">
+
+                <div className="song__image-container">
+
+                    <img
+                        src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24"
+                        alt="Imagem da música x "
+                    />
+
+                </div>
+
+            </div>
+
+            <div className="song__bar"></div>
+
+        </div>
     )
 }
 
