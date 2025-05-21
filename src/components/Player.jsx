d44212e907fe5e4843e796b6760aea77986b23d4
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-21 19:30:39 -0300

    update code

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay, faBackwardStep, faForwardStep } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Player = () => {
     return (
@@ -8,25 +9,41 @@ const Player = () => {
 
             <div className="player__controllers">
 
+                <Link to="/song/1">
+
+                    <FontAwesomeIcon
+                        className="player__icon"
+                        icon={faBackwardStep}
+                    />
+
+                </Link>
+
                 <FontAwesomeIcon
-                    className="palyer__icon"
-                    icon={faBackwardStep}
-                />
-                
-                <FontAwesomeIcon
-                    className="palyer__icon"
+                    className="player__icon player__icon--play"
                     icon={faCirclePlay}
                 />
 
-                <FontAwesomeIcon
-                    className="palyer__icon"
-                    icon={faForwardStep}
-                />
+                <Link to="/song/3">
 
+                    <FontAwesomeIcon
+                        className="player__icon"
+                        icon={faForwardStep}
+                    />
+
+                </Link>
             </div>
 
-            <div className="player__bar">
+            <div className="player__progress">
 
+                <p>00:00</p>
+
+                <div className="player__bar">
+
+                    <div className="player__bar-progress"></div>
+
+                </div>
+
+                <p>02:30</p>
 
             </div>
 
