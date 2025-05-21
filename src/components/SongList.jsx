85b86e09cf4caaa38c23efbbb8ed84e825c61b84
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-21 20:17:29 -0300

    add spreed operator

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -2,16 +2,12 @@ import React from 'react'
 import SongItem from './SongItem'
 
 
-const SongList = () => {
+const SongList = ({ songsArray }) => {
     return (
 
         <div className="song-list">
 
-            <SongItem />
-            <SongItem />
-            <SongItem />
-            <SongItem />
-            <SongItem />
+            {songsArray.map((currentObj, index) => (<SongItem {...currentObj} index={index} key={(index)} />))}
 
             <p className='song-list__see-more'>Ver mais</p>
 
