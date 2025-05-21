a36d1af1227f75548ad7a0c97652267fe69ee74e
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-20 21:21:18 -0300

    duplicate SongItem

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import SongItem from './SongItem'
+
 
 const SongList = () => {
     return (
 
         <div className="song-list">
 
-            
-
+            <SongItem />
+            <SongItem />
+            <SongItem />
+            <SongItem />
+            <SongItem />
 
         </div>
     )
