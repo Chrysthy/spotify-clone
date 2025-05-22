1368b40bea863bc6249bbfa54b72d411806a82d6
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-22 20:53:23 -0300

    add endpoints and import artists and songs

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,10 +1,20 @@
 import express from 'express';
+import { artistArray } from '../../front-end/src/assets/database/artists.js';
+import { songsArray } from '../../front-end/src/assets/database/songs.js';
 
 const app = express();
 const PORT = 3000;
 
 app.get('/', (request, response) => {
-    response.send('Hello World! Utilizando o node --watch ./api/server.js');
+    response.send("'Hello World! Utilizando o node --watch ./api/server.js'");
+})
+
+app.get('/artists', (request, response) => {
+    response.send(artistArray);
+})
+
+app.get('/songs', (request, response) => {
+    response.send(songsArray);
 })
 
 app.listen(PORT, () => {
