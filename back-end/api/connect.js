e8b845782a0939fa74864320fe9cda2d675151d6
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-24 22:07:50 -0300

    add import mongo and new const client

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -1,3 +1,6 @@
-
+import { MongoClient } from "mongodb";
 
 const URI = "mongodb+srv://chrystinemartins:O78VS2hiJQ8AmStw@cluster0.rapadho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+const client = new MongoClient(URI);
+
