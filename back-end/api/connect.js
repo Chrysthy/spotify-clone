705336fd2c8ba34e67b7aa92b28d54bca8fbce77
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-25 12:59:44 -0300

    updace code with new mongo password

---
+++ back-end/api/connect.js
@@ -0,0 +1,15 @@
+import { MongoClient } from "mongodb";
+import 'dotenv/config';
+
+const URI = process.env.Mongo_URI;
+
+const client = new MongoClient(URI);
+
+await client.connect();
+
+export const db = client.db("spotifyAula");
+
+// const songCollection = await db.collection("songs").find({}).toArray();
+
+// console.log(songCollection)
+
