1493797d4168f139ed0667187159cccd01891e73
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-25 17:27:31 -0300

    import cors

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import { db } from './connect.js';
 
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+//transforma em json
 
 app.get('/', (request, response) => {
     response.send("'Hello World! Utilizando o node --watch ./api/server.js'");
