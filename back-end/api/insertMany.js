8690419930a17e1782aac3e9c160caf539c2587a
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-24 22:31:24 -0300

    create new const ArtistArray ...

---
+++ back-end/api/insertMany.js
@@ -0,0 +1,11 @@
+import { artistArray } from '../../front-end/src/assets/database/artists.js';
+import { songsArray } from '../../front-end/src/assets/database/songs.js';
+
+const newArtistArray = artistArray.map((currentArtistObjs) => {
+
+    const newArtistObj = { ...currentArtistObjs };
+    delete newArtistObj.id
+
+    return newArtistObj;
+
+})
