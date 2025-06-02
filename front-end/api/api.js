b9369074633628c089aab31b1d4b38061f0762ad
Chrystine Martins <chrystine.martins@outlook.com>
2025-06-02 19:25:42 -0300

    update code

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,17 +2,29 @@
 //Requisição do front.
 //Pode usar o Fetch ou Axios.
 
-import 'dotenv/config';
-import axios from 'axios';
+// import axios from 'axios';
 
 // const { VITE_NODE_ENV } = import.meta.env;
 // const URL = VITE_NODE_ENV === 'development' ? "http://localhost:3000/api" : '/api';
 
-const URL = "https://spotify-clone-r1jm.onrender.com"
+// const URL = "https://spotify-clone-r1jm.onrender.com"
 
-const responseArtists = await axios.get(`${URL}/artists`);
-const responseSongs = await axios.get(`${URL}/songs`);
+// const responseArtists = await axios.get(`${URL}/artists`);
+// const responseSongs = await axios.get(`${URL}/songs`);
 
-export const artistArray = responseArtists.data;
-export const songsArray = responseSongs.data;
+// export const artistArray = responseArtists.data;
+// export const songsArray = responseSongs.data;
 
+import axios from 'axios';
+
+const URL = "https://spotify-clone-r1jm.onrender.com/api";
+
+export async function fetchArtists() {
+  const response = await axios.get(`${URL}/artists`);
+  return response.data;
+}
+
+export async function fetchSongs() {
+  const response = await axios.get(`${URL}/songs`);
+  return response.data;
+}
